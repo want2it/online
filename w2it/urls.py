9a4b5a759e7b129b96ebec95230c8a4d5abe0cb2
Want2IT.ONLINE <want2it@outlook.com>
2022-09-10 17:41:23 +0300

    Commit 16:52 10/09/2022

--- w2it/urls.py
+++ w2it/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('summernote/', include('django_summernote.urls')),
 ]
