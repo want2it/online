9e991615f5e6550df538e1b2e5b95ec074da921a
Want2IT.ONLINE <want2it@outlook.com>
2022-09-10 19:06:43 +0300

    Commit 19:06 10/09/2022

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,6 +9,11 @@ class Post(models.Model):
     post_image = models.ImageField(upload_to='static/images/blog/')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+       return self.title
+
 
     def publish(self):
         self.published_date = timezone.now()
