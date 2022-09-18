4c232e3e4c0f1074c00d23ef50b85b86768ea277
want2it <want2it@outlook.com>
2022-09-18 20:41:33 +0300

    +++

--- blog/views.py
+++ blog/views.py
@@ -51,17 +51,11 @@ class PostListUpload(generic.ListView):
 
 
 
-def post_spis(request):
-    posts = Post.objects.filter(status=1).order_by('-created_on')[:3]
-    return render(request, 'home.html', {'posts': posts, 'title': 'Хочу в IT · Рекрутинговое агентство', 'meta_1': 'Хочу в IT. Рекрутинговое агентство по подбору IT-специалистов', 'meta_2': 'Рекрутинговое агентство по профессиональному подбору IT-специалистов любого грейда и любой сложности. Мы соберем Вашу команду мечты! Работаем с применением различных техник в части поиска и подбора персонала, используем как классические инструменты, так и современные методики для оценки и поиска сотрудников.', 'meta_3': 'кадровое агентство, резюме, HR, вакансии, IT, работа, подбор персонала, оффер, рекрутер, рекрутинг, дешевый поиск персонала, разработчики, аутстафф, айти, быстрый поиск персонала, rflhjdjt futyncndj, htp.vt, HR, dfrfycbb, IT, hf,jnf, gjl,jh gthcjyfkf, jaath, htrhenth, htrhenbyu, ltitdsq gjbcr gthcjyfkf, hfphf,jnxbrb, fencnfaa, fqnb, ,scnhsq gjbcr gthcjyfkf, recruitment agency, resume, HR, vacancies, IT, job, recruitment, offer, recruiter, recruiting, cheap staff search, developers, outstaff, IT, quick staff search, кускгшеьуте фпутсн, куыгьу, РК, мфсфтсшуы, ШЕ, ощи, кускгшеьуте, щааук, кускгшеук, кускгшештп, сруфз ыефаа ыуфкср, вумудщзукы, щгеыефаа, ШЕ, йгшсл ыефаа ыуфкср'})
 
 
 
 
 
-def blog(request):
-    posts = Post.objects.order_by('-created_on')
-    return render(request, 'blog.html', {'posts': posts, 'title': 'Хочу в IT · Блог', 'meta_1': 'Хочу в IT. Блог для IT-рекрутеров и не только.', 'meta_2': 'Рекрутинговое агентство по профессиональному подбору IT-специалистов любого грейда и любой сложности. Блог, новости, истории и советы.', 'meta_3': 'кадровое агентство, резюме, HR, вакансии, IT, работа, подбор персонала, оффер, рекрутер, рекрутинг, дешевый поиск персонала, разработчики, аутстафф, айти, быстрый поиск персонала, rflhjdjt futyncndj, htp.vt, HR, dfrfycbb, IT, hf,jnf, gjl,jh gthcjyfkf, jaath, htrhenth, htrhenbyu, ltitdsq gjbcr gthcjyfkf, hfphf,jnxbrb, fencnfaa, fqnb, ,scnhsq gjbcr gthcjyfkf, recruitment agency, resume, HR, vacancies, IT, job, recruitment, offer, recruiter, recruiting, cheap staff search, developers, outstaff, IT, quick staff search, кускгшеьуте фпутсн, куыгьу, РК, мфсфтсшуы, ШЕ, ощи, кускгшеьуте, щааук, кускгшеук, кускгшештп, сруфз ыефаа ыуфкср, вумудщзукы, щгеыефаа, ШЕ, йгшсл ыефаа ыуфкср'})
 
 def join(request):
     posts = Post.objects.order_by('-created_on')[:3]
